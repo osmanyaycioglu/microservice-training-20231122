91bba164fff5da8bd004c9802346b321927ba13b
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-11-22 15:29:39 +0300

    day 2 2c

---
+++ ms-agreement/src/main/java/com/micro/training/msagreement/MsAgreementApplication.java
@@ -0,0 +1,14 @@
+package com.micro.training.msagreement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgreementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAgreementApplication.class,
+                              args);
+    }
+
+}
---
+++ ms-agreement/src/test/java/com/micro/training/msagreement/MsAgreementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.training.msagreement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgreementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
