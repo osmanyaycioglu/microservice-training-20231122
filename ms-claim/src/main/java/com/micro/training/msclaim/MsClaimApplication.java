7242ce711a08327d550e58669caee7730128fc49
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-11-24 17:01:31 +0300

    day 4 4

---
+++ ms-claim/src/main/java/com/micro/training/msclaim/MsClaimApplication.java
@@ -0,0 +1,14 @@
+package com.micro.training.msclaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClaimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClaimApplication.class,
+                              args);
+    }
+
+}
---
+++ ms-claim/src/test/java/com/micro/training/msclaim/MsClaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.training.msclaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClaimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
